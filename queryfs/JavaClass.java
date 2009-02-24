5a70a599217c7962464e5ebc07f4d74d2c34fb25
Eric Liang <forward.eric@gmail.com>
2009-02-23 23:50:53 -0800

    initial commit

---
+++ queryfs/JavaClass.java
@@ -0,0 +1,15 @@
+/**
+ * 	Placeholder Class
+ *	@author Eric Liang
+ *	@created Mon Feb 23 23:49:39 PST 2009
+ */
+
+package queryfs;
+
+public class JavaClass {
+	/**
+	 * doc
+	 */
+	public JavaClass() {
+	}
+}
